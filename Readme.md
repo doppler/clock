b069a48a72506eec5acba2bd787a7d1efb7735cd
David Rose <doppler@gmail.com>
2020-01-18 01:15:05 -0500

    open in upper-right of 2nd monitor, for now

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# SimpleClock
+
+Small, translucent digital clock that floats on top of all other apps.
+
+Built with [Electron](https://electronjs.org/).
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, screen } = require('electron');
+
+const width = 115;
+const height = 36;
+let x, y;
 
 const createWindow = () => {
   let win = new BrowserWindow({
-    width: 115,
-    height: 40,
+    width,
+    height,
+    x,
+    y,
     frame: false,
     transparent: true,
     alwaysOnTop: true,
@@ -23,6 +29,13 @@ const createWindow = () => {
 
 app.on('ready', () => {
   setTimeout(createWindow, 300);
+
+  let displays = screen.getAllDisplays();
+  displays.forEach(display => console.log(display));
+
+  let displaySize = displays[1].size;
+  x = displaySize.width - width - height;
+  y = 0;
 });
 
 app.on('window-all-closed', () => {
